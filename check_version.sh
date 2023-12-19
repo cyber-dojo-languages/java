57ee09471925a8fdf333e38f2f655df157bdd98f
Faye <faye@kosli.com>
2023-12-19 10:47:22 +0100

    Try to change from CircleCI to Github Actions and change interactivity of Docker container

--- check_version.sh
+++ check_version.sh
@@ -7,7 +7,7 @@ readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly EXPECTED='javac 21.0.1'
-readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'javac -version 2>&1')
+readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'javac -version 2>&1')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
